0362b49199ab6be32bb7bd26ed9e97ad52a75b7a
jungmin7315 <157206516+jungmin7315@users.noreply.github.com>
2024-01-24 22:52:28 +0900

    문자열.java

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -11,17 +11,12 @@ public class Main{
         for(int i =0; i<t;i++){
             //input단어 문자열 변수
             input = br.readLine();
+            //문자열의 첫번째와 끝 출력 단 문자열의 길이가 1인경우 그 값을 2번 출력
             if(input.length() != 1){
                 System.out.printf("%c%c%n",input.charAt(0),input.charAt(input.length()-1));    
             }else{
                 System.out.printf("%c%c%n",input.charAt(0),input.charAt(0));    
             }
-            
         }
-        
-        
-        //문자열 길이 출력
-        
-        
     }
 }
