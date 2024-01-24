a61b71e723088c6e1f17a5c9cced2ef04e5ee190
jungmin7315 <157206516+jungmin7315@users.noreply.github.com>
2024-01-24 22:25:08 +0900

    [Bronze V] Title: 문자와 문자열, Time: 80 ms, Memory: 11780 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,22 +1,16 @@
 import java.io.*;
-import java.util.*;
 
 public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //input단어 문자열 변수
+        String input = br.readLine();
+        //i번째 글자를 출력받기 위한 변수
+        int i = Integer.parseInt(br.readLine());
         
-        StringTokenizer st;
-        
-        st = new StringTokenizer(br.readLine());
-        //입력받은 문자열 저장하는 변수
-        String input = st.nextToken();
-        
-        st = new StringTokenizer(br.readLine());
-        //입력받은 숫자의 위치 번호 변수
-        int i = Integer.parseInt(st.nextToken());
-        
-        //문자열중에서 원하는 i의 위치만 출력
-        System.out.printf("%s",input.substring(i-1,i));
+        //문자열중에서 원하는 i의 번째만 출력
+        System.out.printf("%s",input.charAt(i-1));
         
     }
 }
