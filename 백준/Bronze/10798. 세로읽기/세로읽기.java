7e4c6627b0cd39f33a07083a6f1c74a6d2745789
jungmin7315 <157206516+jungmin7315@users.noreply.github.com>
2024-01-30 22:43:42 +0900

    [Bronze I] Title: 세로읽기, Time: 80 ms, Memory: 11772 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        String input;
+        char[][] al = new char[5][15];
+        //입력받은 글자들을 배열에 넣어주기
+        for(int i=0;i<5;i++){
+            input = br.readLine();
+            char[] a = input.toCharArray();
+            for(int j=0;j<input.length();j++){
+                al[i][j] = a[j];
+            }
+        }
+        
+        //세로로 읽은 순서대로 출력;
+        for(int j=0;j<15;j++){
+            for(int i=0;i<5;i++){
+                //공백이 있을시 건너뛰기
+                if(al[i][j] == 0){
+                    continue;
+                }else{
+                    sb.append(al[i][j]);
+                }
+            }
+        }
+        
+        System.out.printf("%s",sb);
+        
+        br.close();
+    }
+}
