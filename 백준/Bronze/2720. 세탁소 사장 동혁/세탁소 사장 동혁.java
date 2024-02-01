200ed2d419c5d2935efe111c6f3c7c706d2604b0
jungmin7315 <157206516+jungmin7315@users.noreply.github.com>
2024-02-01 19:51:16 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 12284 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -4,36 +4,27 @@ import java.util.*;
 public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         
         int t = Integer.parseInt(br.readLine());
         
         for(int i=0;i<t;i++){
-            int c = Integer.parseInt(br.readLine())*10;
-            int qu = 0;//쿼터
-            int di = 0;//다임
-            int ni = 0;//니켈
-            int pe = 0;//페니
-            while(true){
-                if(c >= 250){//입력 받은 값이 0.25*100 보다 높으면 쿼터 증가
-                    c -= 250;
-                    qu++;
-                }else if(c >= 100){//입력 받은 값이 0.1*100 보다 높으면 다임 증가
-                    c-=100;
-                    di++;
-                }else if(c >= 50){//입력 받은 값이 0.05*100 보다 높으면 니켈 증가
-                    c-=50;
-                    ni++;
-                }else if(c >= 10){//입력 받은 값이 0.01*100 보다 높으면 페니 증가
-                    c-=10;
-                    pe++;
-                }else if(c < 10){//입렫 받은 값이 0.01*100 보다 작으면 반복문 나가기
-                    break;
-                }
-            }
-            System.out.printf("%d %d %d %d%n",qu,di,ni,pe);//출력
-            
+            //c의 단위는 센트(1달러 = 100센트)
+             int c = Integer.parseInt(br.readLine());
+            //쿼터 25
+            //다임 10
+            //니켈 5
+            //페니 1
+            sb.append(c/25+" ");//c의 값을 쿼터로 나눴을때 몫 출력
+            c %= 25;//나머지는 c에 저장
+            sb.append(c/10+" ");//c의 값을 다임로 나눴을때 몫 출력
+            c %= 10;//나머지는 c에 저장
+            sb.append(c/5+" ");//c의 값을 니켈로 나눴을때 몫 출력
+            c %= 5;//나머지는 c에 저장
+            sb.append(c/1+" ");//c의 값을 페니로 나눴을때 몫 출력
+            sb.append("\n");
         }
-        
+        System.out.printf("%s",sb);
         br.close();
     }
 }
