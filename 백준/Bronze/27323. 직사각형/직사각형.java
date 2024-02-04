666aabee505597e7798891382b0f3282187750b9
jungmin7315 <157206516+jungmin7315@users.noreply.github.com>
2024-02-04 10:59:56 +0900

    [Bronze V] Title: 직사각형, Time: 84 ms, Memory: 11752 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int sum = a*b;
+
+        System.out.printf("%d",sum);
+    }
+}
