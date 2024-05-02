26d684f8c8a30c16e5aa894111116d950a1f31e7
jungmin7315 <157206516+jungmin7315@users.noreply.github.com>
2024-05-02 20:22:21 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 928 ms, Memory: 195736 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -2,50 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int k[];
-    static int[] tmp;
+
+    static int arr[];
+    static int temp[];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder str = new StringBuilder();
 
-        k = new int[Integer.parseInt(st.nextToken())];
-        tmp = new int[k.length];
-
-        for (int i = 0; i < k.length; i++) {
+        int n = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        temp = new int[arr.length];
+        for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine());
-            k[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        mergeSort(0, k.length - 1);
-        for(int i:k){
-            bw.write(i+"\n");
+        mergeSort(0,n-1);
+
+        for(int i:arr){
+//            System.out.println(i);
+            str.append(i+"\n");
         }
-        bw.flush();
-        bw.close();
+
+        System.out.println(str);
+
     }
 
-    public static void mergeSort(int start, int end) {
-        if (start < end) {
-            int mid = (start + end) / 2;
+    public static void mergeSort(int Start, int End){
+        if(End - Start < 1) return;
 
-            mergeSort(start, mid);
-            mergeSort(mid + 1, end);
-            int p = start;
-            int q = mid + 1;
-            int idx = p;
+        int Middle = ( End + Start) / 2;
+        mergeSort(Start, Middle);
+        mergeSort(Middle + 1, End);
 
-            while (p <= mid || q <= end) {
-                if (q > end || (p <= mid && k[p] < k[q])) {
-                    tmp[idx++] = k[p++];
-                } else {
-                    tmp[idx++] = k[q++];
-                }
-            }
-            for (int i = start; i <= end; i++) {
-                k[i] = tmp[i];
-            }
+        for(int i = Start;  i <= End; i++){
+            temp[i] = arr[i];
         }
 
+        int k = Start;
+        int x = Start;
+        int y = Middle + 1;
+        while (x <= Middle && y <= End) {
+            if(temp[x] < temp[y]){
+                arr[k++] = temp[x++];
+            }else{
+                arr[k++] = temp[y++];
+            }
+        }
+        while(x<=Middle){
+            arr[k++] = temp[x++];
+        }
+        while(y<=End){
+            arr[k++] = temp[y++];
+        }
     }
 }
