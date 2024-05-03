7afd607bb97fc4d9210f18608a04a3b304a63d29
jungmin7315 <157206516+jungmin7315@users.noreply.github.com>
2024-05-03 17:25:41 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/README.md
+++ 프로그래머스/0/120818. 옷가게 할인 받기/README.md
@@ -1,6 +1,6 @@
 # [level 0] 옷가게 할인 받기 - 120818 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120818) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120818?language=java) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 03일 17:25:29
+2024년 05월 03일 17:25:38
 
 ### 문제 설명
 
--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,13 +1,6 @@
 class Solution {
     public int solution(int price) {
-        double answer = 0;
-        if(price >= 500000) {
-            answer = price * 0.2;
-        }else if(price >= 300000){
-            answer = price * 0.1;
-        }else if(price >= 100000){
-            answer = price * 0.05;
-        }
-        return (int)(price - answer);
+        int answer = 0;
+        return answer;
     }
 }
