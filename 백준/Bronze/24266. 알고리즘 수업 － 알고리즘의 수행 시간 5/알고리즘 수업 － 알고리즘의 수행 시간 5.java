c35c05798d9fd237f4f7b647c60b9a082a4ede98
jungmin7315 <157206516+jungmin7315@users.noreply.github.com>
2024-05-18 16:36:06 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 116 ms, Memory: 12832 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        long N = scanner.nextInt();
+
+        System.out.println(N*N*N);
+        System.out.println(3);
+    }
+}
