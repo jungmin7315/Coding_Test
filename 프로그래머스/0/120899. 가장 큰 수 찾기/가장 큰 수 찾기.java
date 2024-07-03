c2ad5cd1957e8c54f8d11157a8bffdfb0083bca5
jungmin7315 <157206516+jungmin7315@users.noreply.github.com>
2024-07-03 20:29:30 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = {};
+        int max = 0;
+        int index = 0;
+        for(int i = 0; i<array.length; i++){
+            if(array[i] >= max){
+                max = array[i];
+                index = i;
+            }
+        }
+        answer = new int[]{max, index};
+
+        return answer;
+    }
+}
